3d77375e0bfeaef23ada4df449d3a9029da0a13d
Leo Liang <jlia0010@student.monash.edu>
2020-06-04 22:49:18 +1000

    Complete monitoring window

--- Blood_pressure_monitor.py
+++ Blood_pressure_monitor.py
@@ -28,14 +28,38 @@ class Bp_monitor_window:
 
         for i in range(len(monitoring_list)):
             id = monitoring_list[i]
+            patient = self.practitioner.get_patient(id
+                                                    )
             row = int(i/3)
             column = int(i%3)
 
             frame = tk.Frame(self.window)
             frame.grid(row=row, column=column)
+
+
+            full_bp_array = patient.blood_array["Systolic Blood Pressure"].copy()
+
+            history_text = "Last five systolic blood pressure history for " + patient.get_fullname() + "\n"
+            value_list = []
+            unit_list = []
+            date_list = []
+
+            for i in range(5):
+                latest = full_bp_array.pop(full_bp_array.index(max(full_bp_array)))
+                value = str(latest[1])
+                unit = str(latest[2])
+                date = str(latest[0])
+                value_list.insert(0,value)
+                unit_list.insert(0,unit)
+                date_list.insert(0,date)
+
+            for j in range(5):
+                history_text += value_list[j] + " " + unit_list[j] + " " + date_list[j]
+                if j != 4:
+                    history_text += "\n"
+
+            history_label = tk.Label(master=frame, text=history_text, bg='grey', fg='white')
+            history_label.grid()
+
+
             blood_pressure_history_graph(frame,self.practitioner,id)
-
-
-
-
-
